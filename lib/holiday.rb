1f0a5520567895142f0fbbdf9e700ac79e454477
Samantha Meason <sbmeason@gmail.com>
2020-02-03 22:32:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,8 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
-holiday_hash
+  
+  holiday_hash
 end
 
 
