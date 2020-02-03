e4b934b88e446d317c45af013c05bf13650f67d8
Samantha Meason <sbmeason@gmail.com>
2020-02-03 22:50:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,6 +38,8 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
+end
+end
 
 
 
