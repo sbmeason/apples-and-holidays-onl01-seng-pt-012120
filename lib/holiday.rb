60005d1d94acf3bb918d2e124b76fc57a4c01ffa
Samantha Meason <sbmeason@gmail.com>
2020-02-03 20:53:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -6,6 +6,8 @@ holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  (holiday_supplies)
+  holiday_supplies[:winter][:christmas][:new_years] = "Balloons"
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
