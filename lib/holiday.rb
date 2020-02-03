4be071c0ef85380c5f017f42825899846798e900
Samantha Meason <sbmeason@gmail.com>
2020-02-03 22:25:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,8 @@ holiday_supplies[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
- 
+ (holiday_supplies)
+ holiday_supplies[]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
