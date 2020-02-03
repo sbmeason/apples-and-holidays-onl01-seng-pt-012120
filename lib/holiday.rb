e50b806805022a9469c75665304814cdd661d53a
Samantha Meason <sbmeason@gmail.com>
2020-02-03 21:14:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,6 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_supplies[:fall] = "colombus_day"
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
