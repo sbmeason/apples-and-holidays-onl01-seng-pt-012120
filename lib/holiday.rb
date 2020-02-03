bc459c8c5eccdf1326037196de03a5664fd97758
Samantha Meason <sbmeason@gmail.com>
2020-02-03 20:49:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,21 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_supplies = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
+ 
 second_supply_for_fourth_of_july(holiday_supplies)
 holiday_supplies[:summer][:fourth_of_july][1]
 end
