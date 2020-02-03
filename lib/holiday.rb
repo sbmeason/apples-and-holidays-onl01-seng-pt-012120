1b9f92cd29d0ecd55e5bb86087717eee0484ee1c
Samantha Meason <sbmeason@gmail.com>
2020-02-03 20:49:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,8 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
- 
-second_supply_for_fourth_of_july(holiday_supplies)
+(holiday_supplies)
 holiday_supplies[:summer][:fourth_of_july][1]
 end
 
