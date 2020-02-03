88326f8658d8e2f54f3d1489195234af5e945b8f
Samantha Meason <sbmeason@gmail.com>
2020-02-03 20:59:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -10,6 +10,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_supplies[:winter].each do |holiday, decorations|
     decorations << supply
 end
+end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
